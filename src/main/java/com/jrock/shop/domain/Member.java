ebc220f81b4221a927716b3076fa2dd7421ed262
Jrock30 <jrg8989@knou.ac.kr>
2021-03-28 22:23:46 +0900

    DAY

--- src/main/java/com/jrock/shop/domain/Member.java
+++ src/main/java/com/jrock/shop/domain/Member.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,6 +16,7 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    @NotEmpty
     private String name;
 
     @Embedded // 내장타입 ( 값 타입 ) 여기는 굳이 해줄 필요는 없지만 명확하게 적용하자.
